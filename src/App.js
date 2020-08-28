236d25da5c5e882d40cb4422decdc7afaf7a8011
SLatyankov <Slatiankov@yandex.ru>
2020-08-28 17:56:55 +0400

    удалил всякую ерунду в исходных файлах

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
 import React from 'react';
 import './App.css';
-import Question from './Variables/State';
-import RandomNumber from './Scripts/Random';
-import Button from './Buttons/Button';
 
 function App() {
-  let x = RandomNumber();
-  function newQuestion(params) {
-    x = RandomNumber();
-    console.log(x);
-  }
+
   return (
     <div className="App">
-      <main className="App-main">
-        <h1>Тренировка глаголов</h1>
-        <h2 className= 'question'>{Question[x]}</h2>
-        <Button onClick={newQuestion} />
-      </main>
     </div>
   );
 }
--- src/Buttons/Button.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-import './Button.css'
-export default function Button () {
-  return <button className="button">&#10132;</button>
-}
